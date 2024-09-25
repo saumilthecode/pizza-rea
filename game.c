b427970df84438e9fc6eb33421a9ecc38b54f64b
potato dog man <saumil707@gmail.com>
2024-09-25 22:19:49 +0800

    Update game.c

--- game.c
+++ game.c
@@ -3,7 +3,9 @@
 #include <conio.h>
 #include <dos.h>
 
+
 #define MAX_INPUT 50
+#define MAX_ITEMS 10
 #define SCREEN_WIDTH 80
 #define SCREEN_HEIGHT 25
 #define MAP_WIDTH 11
@@ -23,28 +25,11 @@ PizzeriaRoom pizzeriaRooms[] = {
 };
 
 int currentLocation = 0;
-char ingredientsBag[50] = "";
-int pizzaReady = 0;
-
-char pizzeriaMap[MAP_HEIGHT][MAP_WIDTH];
-
-int roomCoordinates[][2] = {
-    {5, 5},  /* Kitchen */
-    {5, 3},  /* Storage Room */
-    {7, 5},  /* Dining Area */
-    {7, 7}   /* Oven Room */
-};
+char ingredientsBag[MAX_ITEMS][20];
+int itemCount = 0;
 
 void clearScreen() {
-    union REGS regs;
-    regs.h.ah = 0x06;  /* Scroll up function */
-    regs.h.al = 0;     /* Clear entire screen */
-    regs.h.ch = 0;     /* Upper left corner row */
-    regs.h.cl = 0;     /* Upper left corner col */
-    regs.h.dh = 24;    /* Lower right corner row */
-    regs.h.dl = 79;    /* Lower right corner col */
-    regs.h.bh = 0x07;  /* White on black */
-    int86(0x10, &regs, &regs);
+    clrscr();  // Use Turbo C's built-in screen clear function
 }
 
 void gotoxy(int x, int y) {
@@ -57,43 +42,39 @@ void gotoxy(int x, int y) {
 }
 
 void drawBox() {
-    int i;
-    gotoxy(0, 0);
-    putch(218);
-    for (i = 1; i < SCREEN_WIDTH - 1; i++) putch(196);
-    putch(191);
+    int i;  // Declaration moved to the top
 
+    gotoxy(0, 0);
+    putch(218);  // Top-left corner
+    for (i = 1; i < SCREEN_WIDTH - 1; i++) putch(196);  // Top border
+    putch(191);  // Top-right corner
+    
     for (i = 1; i < SCREEN_HEIGHT - 1; i++) {
         gotoxy(0, i);
-        putch(179);
+        putch(179);  // Left border
         gotoxy(SCREEN_WIDTH - 1, i);
-        putch(179);
+        putch(179);  // Right border
     }
-
+    
     gotoxy(0, SCREEN_HEIGHT - 1);
-    putch(192);
-    for (i = 1; i < SCREEN_WIDTH - 1; i++) putch(196);
-    putch(217);
+    putch(192);  // Bottom-left corner
+    for (i = 1; i < SCREEN_WIDTH - 1; i++) putch(196);  // Bottom border
+    putch(217);  // Bottom-right corner
 }
 
 void printWrapped(int x, int y, const char* text, int width) {
     int curX = x, curY = y;
     int len = strlen(text);
-    int i = 0;
-    int j;
+    int i = 0, j;  // Declaration moved to the top
 
     while (i < len) {
         j = 0;
-        while (j < width && i + j < len && text[i + j] != '\n') {
-            j++;
-        }
-
+        while (j < width && i + j < len && text[i + j] != '\n') j++;
+        
         if (i + j < len && text[i + j] != '\n') {
-            while (j > 0 && text[i + j - 1] != ' ') {
-                j--;
-            }
+            while (j > 0 && text[i + j - 1] != ' ') j--;
         }
-
+        
         gotoxy(curX, curY);
         printf("%.*s", j, &text[i]);
         i += j;
@@ -106,114 +87,65 @@ void drawCompass() {
     int compassX = SCREEN_WIDTH - 10;
     int compassY = SCREEN_HEIGHT - 8;
 
+    // North direction
     gotoxy(compassX, compassY);
-    printf("   N   ");
-    gotoxy(compassX, compassY + 1);
-    printf("   |   ");
-    gotoxy(compassX, compassY + 2);
-    printf("W--+--E");
-    gotoxy(compassX, compassY + 3);
-    printf("   |   ");
-    gotoxy(compassX, compassY + 4);
-    printf("   S   ");
-
-    /* Highlight available directions */
     if (pizzeriaRooms[currentLocation].north != -1) {
-        gotoxy(compassX + 3, compassY);
-        printf("\033[1;33mN\033[0m");
+        textattr(YELLOW);  // Yellow for accessible direction
+        printf("   N   ");
+    } else {
+        textattr(RED);     // Red for inaccessible direction
+        printf("   N   ");
     }
-    if (pizzeriaRooms[currentLocation].south != -1) {
-        gotoxy(compassX + 3, compassY + 4);
-        printf("\033[1;33mS\033[0m");
-    }
-    if (pizzeriaRooms[currentLocation].east != -1) {
-        gotoxy(compassX + 6, compassY + 2);
-        printf("\033[1;33mE\033[0m");
-    }
+
+    // Middle vertical bar
+    gotoxy(compassX, compassY + 1);
+    textattr(WHITE);      // Default color for separator
+    printf("   |   ");
+    
+    // West direction
+    gotoxy(compassX, compassY + 2);
     if (pizzeriaRooms[currentLocation].west != -1) {
-        gotoxy(compassX, compassY + 2);
-        printf("\033[1;33mW\033[0m");
-    }
-}
-
-void initializeMap() {
-    int i, j;
-    for (i = 0; i < MAP_HEIGHT; i++) {
-        for (j = 0; j < MAP_WIDTH; j++) {
-            pizzeriaMap[i][j] = ' ';
-        }
-    }
-}
-
-void updateMap() {
-    int i;
-
-    /* Initialize the map to spaces */
-    initializeMap();
-
-    /* Draw rooms */
-    for (i = 0; i < 4; i++) {
-        int x = roomCoordinates[i][0];
-        int y = roomCoordinates[i][1];
-        if (i == currentLocation) {
-            pizzeriaMap[y][x] = 'X';
-        } else {
-            pizzeriaMap[y][x] = 'O';
-        }
+        textattr(YELLOW);  // Yellow for accessible direction
+        printf("W");
+    } else {
+        textattr(RED);     // Red for inaccessible direction
+        printf("W");
     }
 
-    /* Draw connections */
-    for (i = 0; i < 4; i++) {
-        int x1 = roomCoordinates[i][0];
-        int y1 = roomCoordinates[i][1];
+    // Middle plus sign
+    textattr(WHITE);      // Default color for separator
+    printf("--+--");
 
-        if (pizzeriaRooms[i].north != -1) {
-            int northRoom = pizzeriaRooms[i].north;
-            int x2 = roomCoordinates[northRoom][0];
-            int y2 = roomCoordinates[northRoom][1];
-
-            int y;
-            for (y = y2 + 1; y < y1; y++) {
-                pizzeriaMap[y][x1] = '|';
-            }
-        }
-        if (pizzeriaRooms[i].south != -1) {
-            int southRoom = pizzeriaRooms[i].south;
-            int x2 = roomCoordinates[southRoom][0];
-            int y2 = roomCoordinates[southRoom][1];
-
-            int y;
-            for (y = y1 + 1; y < y2; y++) {
-                pizzeriaMap[y][x1] = '|';
-            }
-        }
-        if (pizzeriaRooms[i].east != -1) {
-            int eastRoom = pizzeriaRooms[i].east;
-            int x2 = roomCoordinates[eastRoom][0];
-            int y2 = roomCoordinates[eastRoom][1];
-
-            int x;
-            for (x = x1 + 1; x < x2; x++) {
-                pizzeriaMap[y1][x] = '-';
-            }
-        }
-        if (pizzeriaRooms[i].west != -1) {
-            int westRoom = pizzeriaRooms[i].west;
-            int x2 = roomCoordinates[westRoom][0];
-            int y2 = roomCoordinates[westRoom][1];
-
-            int x;
-            for (x = x2 + 1; x < x1; x++) {
-                pizzeriaMap[y1][x] = '-';
-            }
-        }
+    // East direction
+    if (pizzeriaRooms[currentLocation].east != -1) {
+        textattr(YELLOW);  // Yellow for accessible direction
+        printf("E");
+    } else {
+        textattr(RED);     // Red for inaccessible direction
+        printf("E");
     }
+
+    // Middle vertical bar
+    gotoxy(compassX, compassY + 3);
+    textattr(WHITE);      // Default color for separator
+    printf("   |   ");
+    
+    // South direction
+    gotoxy(compassX, compassY + 4);
+    if (pizzeriaRooms[currentLocation].south != -1) {
+        textattr(YELLOW);  // Yellow for accessible direction
+        printf("   S   ");
+    } else {
+        textattr(RED);     // Red for inaccessible direction
+        printf("   S   ");
+    }
+
+    textattr(WHITE);  // Reset text color to default
 }
 
 void displayCommands() {
-    int commandsX = SCREEN_WIDTH - 25;
-    int commandsY = 1;
-
+    int commandsX = SCREEN_WIDTH - 20;
+    int commandsY = 2;
     gotoxy(commandsX, commandsY);
     printf("Commands:");
     gotoxy(commandsX, commandsY + 1);
@@ -225,100 +157,184 @@ void displayCommands() {
     gotoxy(commandsX, commandsY + 4);
     printf(" west    - Move west");
     gotoxy(commandsX, commandsY + 5);
-    printf(" take item - Take item");
+    printf(" take item - Collect item");
     gotoxy(commandsX, commandsY + 6);
     printf(" quit    - Exit game");
 }
 
-void displayRoom() {
-    int mapX, mapY;
-    int i, j;
-
-    clearScreen();
-    drawBox();
-
-    printWrapped(2, 1, pizzeriaRooms[currentLocation].description, SCREEN_WIDTH - 4);
-
-    gotoxy(2, 3);
-    printf("Exits:");
-    if (pizzeriaRooms[currentLocation].north != -1) printf(" NORTH");
-    if (pizzeriaRooms[currentLocation].south != -1) printf(" SOUTH");
-    if (pizzeriaRooms[currentLocation].east != -1) printf(" EAST");
-    if (pizzeriaRooms[currentLocation].west != -1) printf(" WEST");
-
-    gotoxy(2, 4);
-    printf("Item: %s", pizzeriaRooms[currentLocation].item);
-
-    drawCompass();
-
-    updateMap();
-    gotoxy(2, 9);
-    printf("Pizzeria Map:\n");
-
-    mapX = 2;
-    mapY = 10;
-    for (i = 0; i < MAP_HEIGHT; i++) {
-        gotoxy(mapX, mapY + i);
-        for (j = 0; j < MAP_WIDTH; j++) {
-            printf("%c", pizzeriaMap[i][j]);
+void displayBackpack() {
+    int i;  // Declare the loop variable here
+    gotoxy(2, 12);  // Display backpack below the current item
+    printf("Backpack: ");
+    
+    if (itemCount == 0) {
+        printf("Empty");
+    } else {
+        for (i = 0; i < itemCount; i++) {  // Properly declare and use 'i'
+            if (i > 0) printf(", ");
+            printf("%s", ingredientsBag[i]);
         }
     }
+}
 
-    /* Display commands in the top-right corner */
+void drawMap() {
+    int mapX = (SCREEN_WIDTH - 40) / 2;
+    int mapY = SCREEN_HEIGHT - 10;  // Adjust map position above the input area
+
+    gotoxy(mapX, mapY);
+    textattr(YELLOW);  // Set color for map header
+    printf("Pizzeria Map: ");
+    
+    // Create a visual layout of the rooms and set colors based on movement possibilities
+    gotoxy(mapX, mapY + 1);
+    
+    // Kitchen (K)
+    gotoxy(mapX, mapY + 2);
+    if (pizzeriaRooms[currentLocation].north == 0) {
+        textattr(YELLOW);  // Yellow for north if accessible
+    } else {
+        textattr(WHITE);   // White if inaccessible
+    }
+    printf("   K   \n");
+
+    // Storage Room (S) and Dining Area (D)
+    gotoxy(mapX, mapY + 3);
+    if (pizzeriaRooms[currentLocation].west == 1) {
+        textattr(YELLOW);  // Yellow for west if accessible
+    } else {
+        textattr(WHITE);
+    }
+    printf("S");
+
+    textattr(WHITE);  // Default color for the space between rooms
+    printf("  ");
+    
+    if (pizzeriaRooms[currentLocation].east == 2 || pizzeriaRooms[currentLocation].south == 2) {
+        textattr(YELLOW);  // Yellow for east/south if accessible
+    } else {
+        textattr(WHITE);
+    }
+    printf("D");
+
+    // Oven Room (O)
+    gotoxy(mapX, mapY + 4);
+    if (pizzeriaRooms[currentLocation].south == 3) {
+        textattr(YELLOW);  // Yellow for south if accessible
+    } else {
+        textattr(WHITE);
+    }
+    printf("   O   \n");
+    
+    textattr(WHITE);  // Reset to default color
+}
+
+void displayRoom() {
+    clearScreen();
+    drawBox();
+    
+    // Display room description
+    gotoxy(2, 6);
+    printf("You are in: ");
+    printWrapped(2, 7, pizzeriaRooms[currentLocation].description, SCREEN_WIDTH - 4);
+    
+    // Display current item in the room
+    gotoxy(2, 10);
+    if (strcmp(pizzeriaRooms[currentLocation].item, "none") != 0) {
+        printf("Current Item: %s", pizzeriaRooms[currentLocation].item);
+    } else {
+        printf("Current Item: None");
+    }
+    
+    // Display backpack contents between current item and command input
+    displayBackpack();
+    
+    // Draw compass and commands
+    drawCompass();
     displayCommands();
+    drawMap();  // Draw the map at the bottom
+}
+
+void takeItem() {
+    if (strcmp(pizzeriaRooms[currentLocation].item, "none") != 0) {
+        if (itemCount < MAX_ITEMS) {
+            strcpy(ingredientsBag[itemCount], pizzeriaRooms[currentLocation].item);
+            itemCount++;
+            printf("\nYou took %s!\n", pizzeriaRooms[currentLocation].item);
+            strcpy(pizzeriaRooms[currentLocation].item, "none");  // Remove the item from the room
+        } else {
+            printf("\nBackpack is full!\n");
+        }
+    } else {
+        printf("\nThere is no item to take!\n");
+    }
+}
+
+void move(char direction) {
+    int newLocation = -1;
+
+    switch (direction) {
+        case 'n':
+            newLocation = pizzeriaRooms[currentLocation].north;
+            break;
+        case 's':
+            newLocation = pizzeriaRooms[currentLocation].south;
+            break;
+        case 'e':
+            newLocation = pizzeriaRooms[currentLocation].east;
+            break;
+        case 'w':
+            newLocation = pizzeriaRooms[currentLocation].west;
+            break;
+    }
+    
+    if (newLocation != -1) {
+        currentLocation = newLocation;
+        printf("You move %c.\n", direction);
+    } else {
+        printf("You can't go that way!\n");
+    }
+}
+
+void customDelay(int milliseconds) {
+    delay(milliseconds);  // Adjust this if needed for your environment
+}
+
+void checkWinCondition() {
+    // Implement win condition logic here
 }
 
 int main() {
     char input[MAX_INPUT];
-
-    while (!pizzaReady) {
+    
+    while (1) {
         displayRoom();
-        printf("\nWhat do you want to do? ");
-        gets(input);
-
-        if (strcmp(input, "north") == 0) {
-            if (pizzeriaRooms[currentLocation].north != -1) {
-                currentLocation = pizzeriaRooms[currentLocation].north;
-            } else {
-                printf("You can't go that way.\n");
-            }
-        } else if (strcmp(input, "south") == 0) {
-            if (pizzeriaRooms[currentLocation].south != -1) {
-                currentLocation = pizzeriaRooms[currentLocation].south;
-            } else {
-                printf("You can't go that way.\n");
-            }
-        } else if (strcmp(input, "east") == 0) {
-            if (pizzeriaRooms[currentLocation].east != -1) {
-                currentLocation = pizzeriaRooms[currentLocation].east;
-            } else {
-                printf("You can't go that way.\n");
-            }
-        } else if (strcmp(input, "west") == 0) {
-            if (pizzeriaRooms[currentLocation].west != -1) {
-                currentLocation = pizzeriaRooms[currentLocation].west;
-            } else {
-                printf("You can't go that way.\n");
-            }
-        } else if (strcmp(input, "take item") == 0) {
-            strcat(ingredientsBag, pizzeriaRooms[currentLocation].item);
-            strcat(ingredientsBag, " ");
-            if (strstr(ingredientsBag, "cheese") && strstr(ingredientsBag, "pepperoni") &&
-                strstr(ingredientsBag, "plates") && strstr(ingredientsBag, "pizza dough")) {
-                pizzaReady = 1;
-                printf("You have all the ingredients! Time to bake the pizza!\n");
-            }
-        } else if (strcmp(input, "quit") == 0) {
-            printf("Exiting the game.\n");
+        
+        gotoxy(2, SCREEN_HEIGHT - 2);  // Adjust if necessary based on map position
+        printf("Enter command: ");
+        fgets(input, MAX_INPUT, stdin);
+        input[strcspn(input, "\n")] = '\0';
+        
+        if (strcmp(input, "quit") == 0) {
             break;
+        } else if (strcmp(input, "north") == 0) {
+            move('n');
+        } else if (strcmp(input, "south") == 0) {
+            move('s');
+        } else if (strcmp(input, "east") == 0) {
+            move('e');
+        } else if (strcmp(input, "west") == 0) {
+            move('w');
+        } else if (strcmp(input, "take item") == 0) {
+            takeItem();
+        } else if (strcmp(input, "show backpack") == 0) {
+            displayBackpack();
         } else {
-            printf("I don't understand that.\n");
+            printf("Unknown command. Please try again.\n");
         }
+        
+        checkWinCondition();
+        customDelay(1000);  // 1-second delay between screen updates
     }
-
-    if (pizzaReady) {
-        printf("\nCongratulations! You've collected all the ingredients and baked the perfect pizza!\n");
-    }
-
+    
     return 0;
 }
